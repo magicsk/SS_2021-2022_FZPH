03f5f5bcf6cbeba388b886640338a7e11679cb81
magicsk <magicsk@magicsk.eu>
2022-03-15 16:40:17 +0100

    Initial Commit

---
+++ FZPH/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  FZPH
+//
+//  Created by magic_sk on 15/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
